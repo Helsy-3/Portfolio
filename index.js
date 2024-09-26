8da9a8c8bd6c81e6b9808f1db6379fc5db40ab6d
Helsy-3 <hesydesoglo@gmail.com>
2024-09-26 12:10:24 +0200

    Initial commit après réinitialisation

---
+++ index.js
@@ -0,0 +1,49 @@
+function setProgress(id, percent) {
+                let circle = document.querySelector(`#${id}`).parentNode.parentNode.querySelector('circle');
+                let radius = circle.r.baseVal.value;
+                let circumference = radius * 2 * Math.PI;
+                circle.style.strokeDasharray = `${circumference} ${circumference}`;
+                circle.style.strokeDashoffset = circumference - (percent / 100) * circumference;
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+                setProgress('number-html', 70);
+                setProgress('number-css', 85);
+                setProgress('number-js', 70);
+                setProgress('number-php', 75);
+                setProgress('number-react', 70);
+                setProgress('number-mysql', 80);
+                setProgress('number-postgresql', 75);
+                setProgress('number-vscode', 85);
+                setProgress('number-git', 80);
+                setProgress('number-github', 80);
+});
+
+
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+                event.preventDefault();
+
+                var formData = new FormData(this);
+                var data = {};
+                formData.forEach((value, key) => {
+                                data[key] = value;
+                });
+
+                fetch('VOTRE_URL_DE_DEPLOIEMENT_ICI', {
+                                method: 'POST',
+                                headers: {
+                                                'Content-Type': 'application/json',
+                                },
+                                body: JSON.stringify(data),
+                })
+                                .then(response => response.json())
+                                .then(data => {
+                                                document.getElementById('response').innerText = 'Message envoyé avec succès!';
+                                                document.getElementById('contact-form').reset();
+                                })
+                                .catch((error) => {
+                                                document.getElementById('response').innerText = 'Erreur lors de l\'envoi du message.';
+                                                console.error('Error:', error);
+                                });
+});
